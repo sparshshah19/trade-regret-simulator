cdb63901938a0f5a841f2b2cb8593d483b6ba65d
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2025-12-22 18:38:50 -0500

    commiting base packages and just like the setup aspect of this program

---
+++ src/basecheck.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+
+print("numpy:", np.__version__)
+print("pandas:", pd.__version__)
+print("RandomForestRegressor:", RandomForestRegressor)
+print("environment works")
